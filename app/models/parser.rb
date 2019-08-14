fc4ca9728f23e3706524c67aa6405d743a298448
arielord <arielord@buffalo.edu>
2019-08-14 14:21:00 -0400

    Updated parser to remove parenthesis from words.

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -3,28 +3,17 @@ class Parser
 
   def self.pronunciation_parser()
     cmudict = "/Users/ariel/OneDrive/Flatiron/sinatra_rhymebot/public/pronunciationDictionary/cmudict-0.7b.txt"
-    counter = 0
     File.foreach(cmudict) do |line|
       if line[0] < 'A'
         next
       elsif line[0] > 'Z'
         next
       else
-        counter = counter + 1
         spelling = line.split[0]
+        spelling = spelling[0...spelling.length-3] if spelling[-1] == ')'
         syllables = line.split[1...line.split.length]
         rhyming_syllable = syllables[-1]
         w = Word.create(spelling: spelling, syllables: syllables, rhyming_syllable: rhyming_syllable)
-
-        if counter > 10
-          break
-        end
-
-        # if Rhymebot.rhymes.key?(w.rhyming_syllable)
-        #   Rhymebot.rhymes[w.rhyming_syllable] << w
-        # else
-        #   Rhymebot.rhymes[rhyming_syllable] = [w]
-        # end
       end
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,2 @@
 Parser.pronunciation_parser()
-User.create(first_name:"Ariel", last_name:"Ordonez", email:"arielord@buffalo.edu", password:"lucila")
-User.create(first_name:"Alexandra", last_name:"Compres", email:"alexandracompres.defrank@gmail.com", password:"lucila24")
+# User.create(first_name:"Ariel", last_name:"Ordonez", email:"arielord@buffalo.edu", password:"lucila")
