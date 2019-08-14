3ac86453d4b749d73e423b5e857c9ba23c56b431
arielord <arielord@buffalo.edu>
2019-08-14 18:02:42 -0400

    validates email uniqueness.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name, :email, :password, on: :create
+  validates :email, uniqueness: true
   has_secure_password
 
   has_many :user_words
