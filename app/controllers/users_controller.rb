221bf4142e4342bcb17f679a82a3b256da535694
arielord <arielord@buffalo.edu>
2019-10-19 05:09:08 -0400

    Got rid of clutter. Only views needed for users are login and signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 
   # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
+  get "/users/login" do
+    #show all user_words
+    erb :"/users/login.html"
   end
 
   # GET: /users/new
-  get "/signup" do
-    erb :"/users/new.html"
+  get "/users/signup" do
+    erb :"/users/signup.html"
   end
 
   # POST: /users
@@ -21,27 +22,7 @@ class UsersController < ApplicationController
     if @user.save
       redirect '/login'
     else
-      redirect "/signup"
+      redirect "/users/new"
     end
-  end
-
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
-  end
-
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
-
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
-
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
   end
 end
